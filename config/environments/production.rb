13db5f976fce215e3ff66d5265fdf492c988a9b7
AllenLiu000 <s3653114@student.rmit.edu.au>
2020-05-21 03:19:18 +1000

    Add account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,8 +100,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
+    :user_name      => ENV['app171869197@heroku.com'],
+    :password       => ENV['do5xxyff8842'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
