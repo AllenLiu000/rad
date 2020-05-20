5ad0156eb20d780cc686d29403c93fde9fe0c0b7
AllenLiu000 <s3653114@student.rmit.edu.au>
2020-05-21 03:03:25 +1000

    Add account activation – Tute10

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 's3653114.herokuapp.com'
+  host = 's3653114raddemo.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
